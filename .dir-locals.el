a1d99ba01bb5f7d53701c72ebd84903e8eef9b65
Wanderson Ferreira <wand@hey.com>
2021-07-25 10:08:24 -0300

    test images in repo

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((org-attach-id-dir . "/Users/wferreir/haskell/aprenda-haskell-rapido-e-dificil/images"))))
